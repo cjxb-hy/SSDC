442c294c9daeb587e1729f3e03abc6de0c2d637a
cjxb-hy <34494217+cjxb-hy@users.noreply.github.com>
2025-03-20 11:08:49 +0800

    Update train_cc.py

--- train_cc.py
+++ train_cc.py
@@ -29,13 +29,8 @@ def main():
 
     ori_train_path = '/home/niusijie/all_data/Cirrus_train'
 
-    val_path1 = '/home/niusijie/all_data/Cirrus_val'
-
-
     train_set = DataLoader(dataset=Multi_Data_Loader(train_path, ori_train_path),
                            batch_size=batch_size, shuffle=True, num_workers=2, drop_last=True)
-    val_set1 = DataLoader(dataset=Data_Loader(val_path1, 'val'),
-                          batch_size=1, shuffle=False)
 
     seg_net = UNet(in_channels, num_cls, 'middle').to(device)
     style_net = DomUniS(in_channels).to(device)
@@ -76,55 +71,14 @@ def main():
                 print('Epoch: {:3}, batch: {:3}, c_loss: {:.10f}, cycle_loss: {:.10f}'.format(
                     epoch, batch, loss.item(), cycle_loss.item()))
 
-        # evaluation
-        with torch.no_grad():
-            seg_net.eval()
-            style_net.eval()
+        # save model
+        path = './models/Cirrus/Cirrus_unet_middle.pth'
+        torch.save(seg_net.state_dict(), path)
+        print('Epoch: {:3}, save model:{}'.format(epoch, path))
 
-            val_dice = 0
-            val_dice_list = [0 for i in range(num_cls-1)]
-            val_count_list = [0 for i in range(num_cls-1)]
-
-            for _, val_ori_image, val_label in val_set1:
-
-                val_ori_image, val_label = val_ori_image.to(
-                    device), val_label.to(device)
-
-                val_du_image = style_net(val_ori_image)
-                val_pred, _, _ = seg_net(val_du_image, val_du_image)
-
-                d_list, c_list = dice_eval(val_pred, val_label)
-
-                val_dice_list = [val_dice_list[i] + d_list[i]
-                                 for i in range(len(val_dice_list))]
-                val_count_list = [val_count_list[i] + c_list[i]
-                                  for i in range(len(val_count_list))]
-
-            avg_dice_list = [val_dice_list[i] / val_count_list[i]
-                             for i in range(len(val_dice_list))]
-
-            for i in range(1, num_cls):
-                val_dice += avg_dice_list[i-1]
-            avg_dice = val_dice/(num_cls-1)
-
-            print("Eval at epoch: {:3}, Source eval dice: {:.10f},samples num: {}".format(
-                epoch, avg_dice, val_count_list))
-
-            fluid_name = ['IRF', 'SRF', 'PED']
-            for fname, fdice in zip(fluid_name, avg_dice_list):
-                print("dice_eval_{}:{}".format(fname, fdice))
-
-            # save model
-            if avg_dice > best_dice:
-
-                best_dice = avg_dice
-                path = './models/Cirrus/Cirrus_unet_middle2.pth'
-                torch.save(seg_net.state_dict(), path)
-                print('Epoch: {:3}, save model:{}'.format(epoch, path))
-
-                path = './models/Cirrus/Cirrus_unet_middle_style2.pth'
-                torch.save(style_net.state_dict(), path)
-                print('Epoch: {:3}, save model:{}'.format(epoch, path))
+        path = './models/Cirrus/Cirrus_unet_middle_style.pth'
+        torch.save(style_net.state_dict(), path)
+        print('Epoch: {:3}, save model:{}'.format(epoch, path))
 
 
 if __name__ == "__main__":
